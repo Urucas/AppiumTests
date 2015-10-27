2d06764522ad10b28fd4d26699b6b37c95a5d13e
vrunoa <vruno.alassia@gmail.com>
2015-10-27 19:31:59 -0300

    test if webview_context appears when closing the app from opened app list, and opening it againg from Chrome icon

--- open-browser/test.py
+++ open-browser/test.py
@@ -30,6 +30,50 @@ class OpenBrowserTests(unittest.TestCase):
         # end the session
         self.driver.quit()
 
+
+    def has_tabs(self):
+        tab = self.driver.find_elements_by_class_name("android.widget.TabWidget")
+        if len(tab) == 0:
+            return False
+        return True
+
+    def is_on_app_list(self):
+        tab = self.driver.find_element_by_class_name("android.widget.TabWidget")
+        if tab is None:
+            return False
+
+        els = tab.find_elements_by_class_name("android.widget.TextView")
+        el = None
+        for el in els:
+            if el.get_attribute("text") == "Apps":
+                break
+
+        if el is None:
+            return False
+
+        if el.get_attribute("selected") == "true":
+            return True
+
+        return False
+
+
+    def search_chrome_button(self):
+        els = self.driver.find_elements_by_class_name("android.widget.TextView")
+        el = None
+        for el in els:
+            if el.get_attribute("text") == "Chrome":
+                return el
+
+        return None
+
+
+    def has_chrome_opened(self):
+        els = self.driver.find_elements_by_class_name("android.widget.TextView")
+        for el in els:
+            if el.get_attribute("text") == "Chrome":
+                return True
+        return False
+
     def test_browser_is_opened(self):
         sleep(.5)
         contexts = self.driver.contexts
@@ -43,6 +87,14 @@ class OpenBrowserTests(unittest.TestCase):
         el = self.driver.find_element_by_id("openBrowserBtt")
         self.assertIsNotNone(el)
         el.click()
+        sleep(.3)
+
+        # if the user has 2 browsers then we click on Chrome button
+        el = self.search_chrome_button()
+        if el is not None:
+            el.click()
+
+        # wait a bit for page to load
         sleep(5)
 
         # press home button
@@ -50,33 +102,56 @@ class OpenBrowserTests(unittest.TestCase):
         sleep(1)
 
 
-        """
         # press switch app button
         self.driver.press_keycode(187)
         sleep(1)
-        """
+
+        # close chrome app
+        size = self.driver.get_window_size()
+        x = size["width"] - 100
+        y = size["height"] - 300
+
+        while self.has_chrome_opened():
+            self.driver.swipe(x, y, 10, y, 600)
+            sleep(.8)
+
+        # close chrome app list
+        self.driver.press_keycode(187)
+        sleep(.1)
 
         # click on the launcher icon
         els = self.driver.find_elements_by_class_name("android.widget.TextView")
         el = None
         for el in els:
-            if el.get_attribute("text") == "":
+            if el.get_attribute("text") == "" or el.get_attribute("text") == "Apps":
                 break
 
         self.assertIsNotNone(el)
         el.click()
-        sleep(.3)
+        sleep(.5)
 
-        source = self.driver.page_source
-        # print source
+        if self.has_tabs():
+            el = None
+            while self.is_on_app_list():
+                el = self.search_chrome_button()
+                if el is not None:
+                    break
 
-        # search for chrome button
-        els = self.driver.find_elements_by_class_name("android.widget.TextView")
-        el = None
-        for el in els:
-            if el.get_attribute("text") == "Chrome":
-                break
+                self.driver.swipe(x, y, 10, y, 600)
+                sleep(.6)
 
+
+        else:
+            el = None
+            for i in range(0,4):
+                el = self.search_chrome_button()
+                if el is not None:
+                    break
+
+                self.driver.swipe(x, y/2, 10, y/2, 600)
+                sleep(.6)
+
+        # click on chrome button
         self.assertIsNotNone(el)
         el.click()
         sleep(.3)
